2574c6740eef8931740ca31e2c5d6c42026ce32d
greatEncoder <allenord@Allens-MacBook-Pro-2.local>
2019-02-17 15:04:11 -0800

    addded the input attributes for model, patch, stride, also reset is complete and submission to the backend is completed

--- code-release/ui/krypton_ui/krypton/views.py
+++ code-release/ui/krypton_ui/krypton/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
+import json
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def selectedRegion(request):
+    print request.FILES
+    message = request.POST
+    return JsonResponse({'message': message})
